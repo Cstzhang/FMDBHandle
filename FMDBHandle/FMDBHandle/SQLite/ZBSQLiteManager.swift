947e8dc667f4a2a27b36a0212e535178354cb818
恒信永利 <hengxinyongli@zzbwork.local>
2017-12-01 20:26:00 +0800

    FMDB数据库管理单例

---
+++ FMDBHandle/FMDBHandle/SQLite/ZBSQLiteManager.swift
@@ -0,0 +1,20 @@
+//
+//  ZBSQLiteManager.swift
+//  FMDBHandle
+//
+//  Created by 恒信永利 on 2017/12/1.
+//  Copyright © 2017年 zhangzhengbin. All rights reserved.
+//
+
+import Foundation
+//数据库管理器
+class ZBSQLiteManager {
+    //全局数据库单例
+    static let shared = ZBSQLiteManager()
+    //构造函数 加private防止外部访问
+    private init(){
+        
+    }
+    
+    
+}
