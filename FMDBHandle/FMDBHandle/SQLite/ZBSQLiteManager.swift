231f67fec08eb9a7962ace8d2e99b9707cec0ae1
恒信永利 <hengxinyongli@zzbwork.local>
2017-12-02 09:29:47 +0800

    创建数据库队列

--- FMDBHandle/FMDBHandle/SQLite/ZBSQLiteManager.swift
+++ FMDBHandle/FMDBHandle/SQLite/ZBSQLiteManager.swift
@@ -5,14 +5,28 @@
 //  Created by 恒信永利 on 2017/12/1.
 //  Copyright © 2017年 zhangzhengbin. All rights reserved.
 //
-
+/*
+ SQLite管理器
+ */
 import Foundation
-//数据库管理器
+import FMDB
+
 class ZBSQLiteManager {
     //全局数据库单例
     static let shared = ZBSQLiteManager()
+    //数据库队列
+    let queue:FMDatabaseQueue
+    
     //构造函数 加private防止外部访问
     private init(){
+       //数据的路径 -path
+        let dbName  = "zbDatabase.db"
+        var path  = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        path = (path as NSString).appendingPathComponent(dbName)
+        print("db path : \(path)")
+        //创建数据库队列 同事创建或者打开数据库
+        queue = FMDatabaseQueue(path: path)
+        
         
     }
     
